600b4b7edb24b06f7241fb564dc1a43b0993aff7
Petez <petersamuelfabonan@gmail.com>
2026-08-19 21:01:55 +0800

    Bulong

--- app/config/routes.php
+++ app/config/routes.php
@@ -44,11 +44,8 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 /** @var object $router **/
 
-$router->get('/', 'StudentController::index');
 
-// Student home page
-$router->get('/student', 'StudentController::index');
- 
 
- 
-$router->get('/student/profile', 'StudentController::profile')->middleware('StudentMiddleware');
+
+$router->get('/student', 'StudentController::index', ['middleware' => 'StudentMiddleware']);
+$router->get('/student/profile', 'StudentController::profile', ['middleware' => 'StudentMiddleware']);
